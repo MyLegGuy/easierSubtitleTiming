859ea069d171c75f9108d5fcb9ccd924113c5061
MyLegGuy <supermoronslayer@gmail.com>
2019-03-10 21:15:23 -0400

    add test audio visualisation

--- main.c
+++ main.c
@@ -2,23 +2,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <ctype.h>
 #include <float.h>
+#include <pthread.h>
 
 #include <sndfile.h>
-
 #include <SDL2/SDL.h>
 
-#define	BLOCK_SIZE 4096
+#include "goodLinkedList.h"
+
+#define PLAY_SAMPLES  4096
 
 #define READBLOCKSIZE 8192
 
+struct sentence{
+	long startSample;
+	long endSample;
+}
+
 //https://gist.github.com/armornick/3447121
-//float buf [BLOCK_SIZE];
+//float buf [PLAY_SAMPLES];
 float** pcmData=NULL;
 long pcmPlayPos=0;
+long totalSamples;
+int sampleRate;
 
+uint32_t audioTimeReference;
+uint32_t audioTimeOther;
+
+pthread_mutex_t audioPosLock;
+
+void unpauseMusic(){
+    	audioTimeReference=SDL_GetTicks();
+	SDL_PauseAudio(0);
+}
+void pauseMusic(){
+	SDL_PauseAudio(1);
+	audioTimeOther+=SDL_GetTicks()-audioTimeReference;
+}
 void my_audio_callback(void *userdata, Uint8 *stream, int len) {
+	pthread_mutex_lock(&audioPosLock);
+
+	if (pcmPlayPos>=totalSamples){
+		pauseMusic();
+		return;
+	}
+    
 	SF_INFO* _passedInfo = userdata;
 	//while ((readcount = sf_readf_float (infile, buf, frames)) > 0)
 	//{
@@ -32,11 +62,16 @@ void my_audio_callback(void *userdata, Uint8 *stream, int len) {
 	//		}
 	//	}
 	//}
-	
-		// HEY, LIUSTEN
 	//readcount = sf_readf_float (userdata, buf, frames);
+	int _possibleWriteSamples = (len/sizeof(float))/2;
+	int _shouldWriteSamples;
+	if (pcmPlayPos+_possibleWriteSamples>totalSamples){
+		_shouldWriteSamples=totalSamples-pcmPlayPos;
+	}else{
+    		_shouldWriteSamples = _possibleWriteSamples;
+	}
 	int i;
-	for (i=0;i<(len/sizeof(float))/2;++i){
+	for (i=0;i<_shouldWriteSamples;++i){
 		int j;
 		for (j=0;j<_passedInfo->channels;++j){
 			SDL_memcpy (&(stream[(i*_passedInfo->channels+j)*sizeof(float)]), &(pcmData[j][pcmPlayPos]), sizeof(float));
@@ -45,8 +80,11 @@ void my_audio_callback(void *userdata, Uint8 *stream, int len) {
 		//stream[(i*2+1)*sizeof(float)] = pcmData[1][pcmPlayPos];
 		++pcmPlayPos;
 	}
-	//SDL_memcpy (stream, pcmData[pcmPlayPos], BLOCK_SIZE*sizeof(float));
-	//pcmPlayPos+=BLOCK_SIZE;
+	if (_shouldWriteSamples!=_possibleWriteSamples){
+		memset(&(stream[_shouldWriteSamples]),0,_possibleWriteSamples-_shouldWriteSamples);
+	}
+	//SDL_memcpy (stream, pcmData[pcmPlayPos], PLAY_SAMPLES*sizeof(float));
+	//pcmPlayPos+=PLAY_SAMPLES;
 	
 
 	//SDL_MixAudio(stream, buf, readcount*sizeof(float), SDL_MIX_MAXVOLUME);
@@ -57,13 +95,36 @@ void my_audio_callback(void *userdata, Uint8 *stream, int len) {
 	//len = ( len > audio_len ? audio_len : len );
 	////SDL_memcpy (stream, audio_pos, len); 					// simply copy from one buffer into the other
 	//SDL_MixAudio(stream, audio_pos, len, SDL_MIX_MAXVOLUME);// mix from one buffer into another
-	//
-	//audio_pos += len;
-	//audio_len -= len;
+
+	pthread_mutex_unlock(&audioPosLock);
+}
+
+void seekAudioSamples(int _numSamples){
+	pthread_mutex_lock(&audioPosLock);
+	pcmPlayPos+=_numSamples;
+	if (pcmPlayPos<0){
+		pcmPlayPos=0;
+	}else if (pcmPlayPos+PLAY_SAMPLES>totalSamples){
+		pcmPlayPos=totalSamples-PLAY_SAMPLES;
+	}
+	audioTimeOther=(pcmPlayPos/(double)sampleRate)*1000;
+	audioTimeReference=SDL_GetTicks();
+	pthread_mutex_unlock(&audioPosLock);
+}
+void seekAudioMilli(int _numMilliseconds){
+    seekAudioSamples((_numMilliseconds/(double)1000)*sampleRate);
+}
+long getCurrentSample(){
+    return ((audioTimeOther+(SDL_GetTicks()-audioTimeReference))/(double)1000)*sampleRate;
 }
 
 int main (int argc, char * argv []){
-	// init
+	if (pthread_mutex_init(&audioPosLock,NULL)!=0){
+		printf("mutex init failed");
+		return 1;
+	}
+    
+	// init audio
 	char* infilename = "./audiodump2.wav";
 	SNDFILE* infile = NULL;
 	SF_INFO	_audioInfo;
@@ -73,6 +134,8 @@ int main (int argc, char * argv []){
 		printf("%s\n",sf_strerror (NULL));
 		return 1;
 	}
+	totalSamples=_audioInfo.frames;
+	sampleRate=_audioInfo.samplerate;
 	// info
 	printf("# Converted from file %s.\n", infilename);
 	printf("# Channels %d, Sample rate %d\n", _audioInfo.channels, _audioInfo.samplerate);
@@ -94,7 +157,7 @@ int main (int argc, char * argv []){
 			int j;
 			for (j = 0; j < _audioInfo.channels; j++){
 			
-				pcmData[j][_wroteSamples] = _readBuf[k*_audioInfo.channels+j];
+				pcmData[j][_wroteSamples] = _readBuf[i*_audioInfo.channels+j];
 				//if (full_precision)
 				//	
 				//else
@@ -116,21 +179,59 @@ int main (int argc, char * argv []){
 	wav_spec.freq = _audioInfo.samplerate;
 	wav_spec.format = AUDIO_F32LSB;
 	wav_spec.channels = _audioInfo.channels;
-	wav_spec.samples = BLOCK_SIZE;
+	wav_spec.samples = PLAY_SAMPLES;
 	wav_spec.callback = my_audio_callback;
 	wav_spec.userdata = &_audioInfo;
-	if ( SDL_OpenAudio(&wav_spec, NULL) < 0 ){
+	int _gottenAudioId = SDL_OpenAudio(&wav_spec, NULL);
+	if ( _gottenAudioId < 0 ){
 		printf("Couldn't open audio: %s\n", SDL_GetError());
 		return 1;
 	}
-	SDL_PauseAudio(0);
-	SDL_Delay(5000);
-	SDL_CloseAudio();
 
-	//float buf [BLOCK_SIZE];
+
+
+
+
+
+
+
+
+
+	/*
+
+	unpauseMusic();
+
+	/////////////////////////////
+
+
+	long _myRef = SDL_GetTicks();
+
+	//SDL_Delay(3000);
+	//seekAudioSamples(-44100);
+	//SDL_Delay(1000);
+	//SDL_CloseAudio();
+	printf("\n");
+	for (int i=0;i<20000;++i){
+
+    		printf("\033[A\r",27);
+    		int _sampleIndex=getCurrentSample();
+    		int j;
+    		for (j=0;j<20*fabs(pcmData[0][_sampleIndex]);++j){
+			printf("=");
+    		}
+    		for (;j<20;++j){
+			printf(" ");
+    		}
+		printf("\n");
+    		//printf("%f\n",pcmData[0][getCurrentSample()]);
+		SDL_Delay(1);
+	}
+	*/
+
+	//float buf [PLAY_SAMPLES];
 	//sf_count_t frames;
 	//int k, m, readcount;
-	//frames = BLOCK_SIZE / _audioInfo.channels;
+	//frames = PLAY_SAMPLES / _audioInfo.channels;
 	//while ((readcount = sf_readf_float (infile, buf, frames)) > 0)
 	//{	for (k = 0; k < readcount; k++) // read wave
 	//	{	for (m = 0; m < _audioInfo.channels; m++){
@@ -143,6 +244,8 @@ int main (int argc, char * argv []){
 	//	}
 	//}
 
+	// whatever the opposite of init is
+	pthread_mutex_destroy(&audioPosLock);
 	
 	return 0;
 }
